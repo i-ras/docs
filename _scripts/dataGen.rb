26febd662d6bc1a1ceb62ac532f4d820518db0f2
Vojtěch Raška <raska.vojtech@gmail.com>
2024-09-15 10:33:11 +0200

    Update

--- _scripts/dataGen.rb
+++ _scripts/dataGen.rb
@@ -2,7 +2,7 @@ require 'fileutils'
 require 'yaml'
 
 subjects_dir = '../_subjects'
-docs_dir = '../docs'
+docs_root_dir = 'docs'
 data_dir = '../_data'
 
 FileUtils.mkdir_p(data_dir)
@@ -12,32 +12,32 @@ Dir.foreach(subjects_dir) do |file|
 
   if file.end_with?('.md')
     subject_name = File.basename(file, '.md')
+    subject_docs_path = File.join(docs_root_dir, subject_name, 'docs')
 
-    subject_path = File.join(docs_dir, subject_name)
-
-    unless Dir.exist?(subject_path)
-      puts "Adresář #{subject_path} neexistuje, přeskočeno."
+    unless Dir.exist?(subject_docs_path)
+      puts "Adresář #{subject_docs_path} neexistuje, přeskočeno."
       next
     end
 
-    data = {
-      'presentation' => [],
-      'worksheet' => [],
-      'test' => []
-    }
+    data = {}
 
     %w[presentation worksheet test].each do |sub_dir|
-      sub_path = File.join(subject_path, sub_dir)
+      sub_path = File.join(subject_docs_path, sub_dir)
 
       if Dir.exist?(sub_path)
+        documents = []
         Dir.foreach(sub_path) do |sub_file|
           next if sub_file == '.' || sub_file == '..'
 
-          data[sub_dir] << {
+          documents << {
             'name' => File.basename(sub_file, File.extname(sub_file)),
-            'path' => File.join(sub_dir, sub_file)
+            'path' => File.join('/', docs_root_dir, subject_name, 'docs', sub_dir, sub_file)
           }
         end
+
+        data[sub_dir] = documents.empty? ? nil : documents
+      else
+        data[sub_dir] = nil
       end
     end
 
@@ -46,7 +46,6 @@ Dir.foreach(subjects_dir) do |file|
     File.open(yaml_file_path, 'w') do |yaml_file|
       yaml_file.write(data.to_yaml)
     end
-
     puts "Vytvořen YAML soubor pro #{subject_name}: #{yaml_file_path}"
   end
 end
