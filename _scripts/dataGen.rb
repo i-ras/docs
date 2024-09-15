75f88e800f77f743a27b3f02f2ab36c658c6cc11
Vojtěch Raška <raska.vojtech@gmail.com>
2024-09-15 10:47:48 +0200

    Update.

--- _scripts/dataGen.rb
+++ _scripts/dataGen.rb
@@ -3,7 +3,7 @@ require 'yaml'
 
 subjects_dir = '../_subjects'
 root_dir = 'docs'
-docs_dir = '../docs'
+docs_dir = 'docs'
 data_dir = '../_data'
 
 FileUtils.mkdir_p(data_dir)
@@ -13,7 +13,7 @@ Dir.foreach(subjects_dir) do |file|
 
   if file.end_with?('.md')
     subject_name = File.basename(file, '.md')
-    subject_docs_path = File.join(docs_dir, subject_name)
+    subject_docs_path = File.join('../', docs_dir, subject_name)
 
     unless Dir.exist?(subject_docs_path)
       puts "Adresář #{subject_docs_path} neexistuje, přeskočeno."
@@ -32,7 +32,7 @@ Dir.foreach(subjects_dir) do |file|
 
           documents << {
             'name' => File.basename(sub_file, File.extname(sub_file)),
-            'path' => File.join('/', docs_dir, subject_name, sub_dir, sub_file)
+            'path' => File.join('/', root_dir, docs_dir, subject_name, sub_dir, sub_file)
           }
         end
 
